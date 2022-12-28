7d07f01b25d912ea207b2c7e93880ae0316d7e14
“Cecilie <ceci316e@stud.kea.dk>
2022-12-28 18:37:34 +0100

    footer

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*** Når man klikker på menu kommer nav op med alle punkterne ***/
 function onClickMenu() {
   document.getElementById("menu").classList.toggle("change");
   document.getElementById("nav").classList.toggle("change");
