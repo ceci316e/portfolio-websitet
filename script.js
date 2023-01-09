5a74736c392b4c10d3d11fec6bc4d4320b3eeb3e
“Cecilie <ceci316e@stud.kea.dk>
2023-01-09 13:22:23 +0100

    slideshow og knap på tema 3

--- script.js
+++ script.js
@@ -1,5 +1,48 @@
 window.addEventListener("load", sidenVises);
 
+//*********************** Slideshow ************************//
+//billeder
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// pile kontroller
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// dutter
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+// let giver dig mulighed for variabler
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  // Gør så billederne køre i ring, ikke bare stopper efter billede 4
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  // Ved ikke hvad gør
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  // Gør så billederne ikke kommer nedenunder hinanden, men kommer efter hinanden
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  // Dutternes farver føler med iforhold til hvilket billede der er blevet klikket på
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  // Billederne
+  slides[slideIndex - 1].style.display = "block";
+  // Dutterne
+  dots[slideIndex - 1].className += " active";
+}
+
+//*********************** Burgermenu ************************//
+
 //Når man klikker på menu kommer nav op med alle punkterne //
 function onClickMenu() {
   document.getElementById("menu").classList.toggle("change");
@@ -7,6 +50,8 @@ function onClickMenu() {
 }
 // opretter variabel minRand
 let minRand;
+
+//*********************** Stjerne på index ************************//
 
 function sidenVises() {
   //Tjekker siden vises og kodningen går igennem
